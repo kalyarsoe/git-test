467d94028917b5c79c28f5971bb75e60c8ad9875
kalyar <kalyar@gmail.com>
2019-11-07 18:18:58 +0630

    Add new file

--- world.php
+++ world.php
@@ -1,14 +1,3 @@
-  function SaveAttendancePerfectData(){    	
-    	document.getElementById("error").innerHTML   = '';
-        document.getElementById("success").innerHTML = '';
-        var hdSelectedEmpList = document.getElementById('hdSelectedEmpList');
-        var chk= true;
-        
-        if(chk){
-                document.forms[0].action = "SavePerfectAttendanceRegister";       
-                document.forms[0].method = "POST";
-                document.forms[0].submit();
-                return true;
-         }
-
-    }
+<? php
+	echo "hello";
+?>
